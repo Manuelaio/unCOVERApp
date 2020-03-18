b3f65d6898704e6bcfbc1ce0640f7c4aa0029202
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2020-03-18 16:41:50 +0100

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -3,7 +3,7 @@ setRepositories(ind = 1:5)
 install.packages(c('shiny', 'shinydashboard'), repos='http://cran.rstudio.com/')
 install.packages('BiocManager')
 BiocManager::install('BiocInstaller')
-list_of_packages <- c("shinyWidgets", "BiocInstaller","shinyWidgets","shinyBS","markdown", "data.table", "DT","dplyr", "Gviz", "Homo.sapiens",
+list_of_packages <- c("shinyWidgets","methods", "BiocInstaller","shinyWidgets","shinyBS","markdown", "data.table", "DT","dplyr", "Gviz", "Homo.sapiens",
                       "OrganismDbi", "stringr", "shinyjs", "condformat","ggbio", "bedr", "org.Hs.eg.db", "rtracklayer","stats",
                       "Rsamtools", "TxDb.Hsapiens.UCSC.hg19.knownGene","TxDb.Hsapiens.UCSC.hg38.knownGene",
                       "BSgenome.Hsapiens.UCSC.hg19", "BSgenome.Hsapiens.UCSC.hg38",
