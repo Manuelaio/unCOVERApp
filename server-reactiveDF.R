a0b421830a0492114c7f2cbaab52c4e34aa310ad
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2020-04-20 12:21:36 +0200

    add example

--- server-reactiveDF.R
+++ server-reactiveDF.R
@@ -8,52 +8,61 @@ txdb= reactive({if (input$UCSC_Genome == "hg19"){
 
 #################make reactive dataframe given input file
 
-value=reactiveValues()
-mydata <- reactive ({
-  inFile<-input$file1
-  i= input$Sample
-  if (is.null(input$file1)) {return(NULL)}
-  df<- read.table(inFile$datapath, header = input$header, stringsAsFactors = F)
-  colnames(df)[1:3]=c("chromosome","start","end")
-  colnames(df)[-1:-3]=paste0("sample_", head(seq_along(df),-3))
-  value= df
+mydata <- reactiveVal()
+observeEvent(input$file1, {
+  
+  tmp <- read.table(input$file1$datapath,
+                    header = input$header, stringsAsFactors = FALSE)
+  colnames(tmp)[1:3]=c("chromosome","start","end")
+  colnames(tmp)[-1:-3]=paste0("sample_", head(seq_along(tmp),-3))
+  ## do whatever is needed to parse the data
+  mydata(tmp)
+})
+
+observeEvent(input$example_data, {
+  polg= read.table("./www/POLG.example.bed.gz")
+  colnames(polg)= c("chromosome","start","end","sample_1")
+  mydata(polg)
 })
 
 ###Output of loaded file 
 
+
 output$text<- #DT::renderDataTable({
-  renderDataTable({
-  validate(need(input$file1 != "", "Please, upload your file"))
-  mydata()})
+  DT::renderDataTable({
+    #validate(need(input$file1 != "", "Please, upload your file"))
+    validate(need(ncol(mydata()) != "0", "Please, upload your file"))
+    mydata()})
 
 ###make reactive dataset given input choosed by users
 
 mysample<-reactive({
   i= input$Sample
+  #print(mydata())
   mydata() %>%
     dplyr:: select(chromosome, start, end,i) %>%
     dplyr::rename(value=i)
 })
 
 filtered_low<- reactive ({
-  validate(
-    need(input$file1 != "", "Unrecognized data set: Please upload your file")
-  )
+  #print(mysample())
+  # validate(
+  #need(input$file1 != "", "Unrecognized data set: Please upload your file")
+  #)
   mysample() %>%
-    dplyr:: filter(chromosome == input$Chromosome, 
+    dplyr:: filter(chromosome == input$Chromosome,
                    value <= as.numeric(input$coverage_co))
 })
 filtered_high<- reactive ({
   mysample() %>%
-    dplyr:: filter(chromosome == input$Chromosome, 
+    dplyr:: filter(chromosome == input$Chromosome,
                    value > as.numeric(input$coverage_co))
   
 })
 
 #######output dateset filtered 
-
 output$text_cv <- DT::renderDataTable({
-  validate(
-    need(input$Gene_name != "" & input$Sample !="", "Please select all required input: Gene, Chromosome, Coverage threshold and Sample")
-  )
+  validate(need(input$Gene_name != "" & input$Sample !="",
+                "Please select all required input: Gene, Chromosome,
+                  Coverage threshold and Sample"))
   filtered_low()})
